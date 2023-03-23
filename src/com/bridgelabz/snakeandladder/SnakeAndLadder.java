5699f135f60444b49dca27a16612a08fb2e34e4c
altaf <altaf98khan66@gmail.com>
2023-03-23 23:47:11 +0530

    Added: Player rolls the dice to get a number between 1 to 6

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -2,9 +2,16 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		
-		 int player_Position=0;
-		    System.out.println("Player's starting position is: " +player_Position );
-	}
 
+		int player_Position = 0;
+		System.out.println("Player's starting position is: " + player_Position);
+
+		// simulate rolling the die 6 times
+		for (int i = 0; i < 5; i++) {
+
+			// After rolling a dice player gets random number between 1 to 6
+			int dice = (int) Math.floor(Math.random() * 6) + 1;
+			System.out.println("After rolling a dice, we get : " + dice);
+		}
+	}
 }
