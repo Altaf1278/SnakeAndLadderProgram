aed404cdeada74838a4e76f7b54f8513f01cf7e9
altaf <altaf98khan66@gmail.com>
2023-03-23 23:36:44 +0530

    Added: Single player at start position 0

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		
+		 int player_Position=0;
+		    System.out.println("Player's starting position is: " +player_Position );
+	}
+
+}
